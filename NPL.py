12de18337c44861c36bb9da9d61ac92430c4ce24
Bruno Guilherme Lunardi <nardi273@gmail.com>
2021-02-26 21:52:55 -0300

    [EDIT] Função para remover stopwords e aplicar stemmer

--- NPL.py
+++ NPL.py
@@ -9,7 +9,11 @@ import pandas as pd
 import nltk
 from sklearn.model_selection import train_test_split
 
-df = pd.read_excel("teste_smarkio_lbs.xls", sheet_name="NLP")
+#nltk.download()
+
+df = pd.read_excel("teste_smarkio_lbs.xls", sheet_name="NLP", header=1)
+
+#print(df.head())
 
 #dividir a base de dados em letras das musicas e o artista a qual as letras pertencem
 musicas = df.iloc[:, 0].values
@@ -22,5 +26,29 @@ previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = tra
 
 #remove as stopswords 
 stopwordsnltk = nltk.corpus.stopwords.words('english')
-# print(stopwordsnltk)
 
+################## início radical das palavras -> stemming e remove stopwords ##################
+def aplicastemmer(musica):
+    """
+      Função que deixa somente radicais das palavras (PorterStemmer) e retira stopwords, ou seja, palavras
+      que são irrelevantes para o processamento do algortimo
+    """
+    
+    #definição do stemmer para deixar radicais das palavras
+    stemmer = nltk.PorterStemmer()
+    frasesstemming = []
+    for palavras in musicas:
+        #para cada palavra que não está na lista de stopwords, remove o radical deste e adiciona na lista
+        comstemming = [str(stemmer.stem(p)) for p in palavras.split() if p not in stopwordsnltk]
+        frasesstemming.append((comstemming))
+        
+    return frasesstemming
+
+teste5 = []
+teste5 = aplicastemmer(previsores_treinamento)
+print(teste5)
+################## fim radical das palavras -> stemming ##################
+
+################## inicio buscar todas as palavras distintas da base de dados ##################
+
+################## fim buscar todas as palavras distintas da base de dados ##################
